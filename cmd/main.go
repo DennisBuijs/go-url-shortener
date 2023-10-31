cd5dd5e999f0046a0a11a42f70e906cdc9208b8b
Dennis Buijs <dennis.buijs@cm.com>
2023-10-31 13:07:02 +0100

    Setup basic server with a status endpoint for monitoring

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,12 @@
 package main
 
+import "net/http"
+
 func main() {
+	http.HandleFunc("/status", statusHandler)
+	http.ListenAndServe("127.0.0.1:3000", nil)
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
