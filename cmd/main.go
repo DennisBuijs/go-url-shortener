c7a3d75e61cdb51146af9b63346659cc52cdde80
Dennis Buijs <dennis.buijs@cm.com>
2023-10-31 13:28:35 +0100

    Add some additional routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -10,6 +11,10 @@ func main() {
 
 	router.HandleFunc("/status", statusHandler)
 
+	router.HandleFunc("/{code}", redirectHandler)
+
+	router.HandleFunc("/web/url", postUrlHandler)
+
 	http.Handle("/", router)
 	http.ListenAndServe("127.0.0.1:3000", router)
 }
@@ -18,6 +23,27 @@ func statusHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+
+	code := mux.Vars(r)["code"]
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Code: %s", code)
+}
+
+func postUrlHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+
+	// Handle form submission
 
 	w.WriteHeader(http.StatusOK)
 }
