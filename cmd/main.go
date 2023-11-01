77dafd12b33270a2695cf359616caa9ce35adccd
Dennis Buijs <dennis.buijs@cm.com>
2023-11-01 17:46:57 +0100

    Add connection to database, implement SQLite

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/gorilla/mux"
 	"html/template"
+	"log"
 	"net/http"
-	"url-shortener/internal/adapters/secondary/memory"
+	"url-shortener/internal/adapters/secondary/sqlite"
+	"url-shortener/internal/ports"
 	"url-shortener/internal/ports/web"
 	"url-shortener/internal/use_cases"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	urlRepository, _ := memory.NewUrlRepository()
+	db, err := sql.Open("sqlite3", "../data/database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	urlRepository, _ := sqlite.NewUrlRepository(db)
 	createUrlUseCase := use_cases.NewCreateUrlUseCase(urlRepository)
 	webInterface := &web.WebInterface{CreateUrlUseCase: createUrlUseCase}
 
@@ -18,11 +30,11 @@ func main() {
 
 	router.HandleFunc("/status", statusHandler)
 
-	router.HandleFunc("/{code}", redirectHandler)
-
 	router.HandleFunc("/", indexPageHandler)
 	router.HandleFunc("/web/url", webInterface.CreateUrlViaWebUiHandler).Methods(http.MethodPost)
 	router.HandleFunc("/api/url", webInterface.CreateUrlViaApiHandler).Methods(http.MethodPost)
+
+	router.HandleFunc("/{code}", redirectHandler(urlRepository))
 
 	fs := http.FileServer(http.Dir("../internal/web/public/"))
 	router.PathPrefix("/public/").Handler(http.StripPrefix("/public/", fs))
@@ -39,22 +51,23 @@ func statusHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func redirectHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
+func redirectHandler(urlRepository ports.UrlRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
 
-	urlRepository, _ := memory.NewUrlRepository()
-	code := mux.Vars(r)["code"]
-	url, err := urlRepository.FindByCode(code)
+		code := mux.Vars(r)["code"]
+		url, err := urlRepository.FindByCode(code)
 
-	if err != nil {
-		http.Error(w, "URL with code '"+code+"' not found", http.StatusNotFound)
+		if err != nil {
+			http.Error(w, "URL with code '"+code+"' not found", http.StatusNotFound)
+			return
+		}
+
+		http.Redirect(w, r, url.Url, http.StatusMovedPermanently)
 		return
 	}
-
-	http.Redirect(w, r, url.Url, http.StatusMovedPermanently)
-	return
 }
 
 func indexPageHandler(w http.ResponseWriter, r *http.Request) {
--- internal/adapters/secondary/sqlite/url.go
+++ internal/adapters/secondary/sqlite/url.go
@@ -3,6 +3,8 @@ package sqlite
 import (
 	"database/sql"
 	"errors"
+	"fmt"
+	"log"
 	"url-shortener/internal/core/domain"
 )
 
@@ -10,17 +12,46 @@ type UrlRepository struct {
 	db *sql.DB
 }
 
-func NewUrlRepository() (*UrlRepository, error) {
-	return &UrlRepository{}, nil
+func NewUrlRepository(db *sql.DB) (*UrlRepository, error) {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS url (id INTEGER PRIMARY KEY, url TEXT, code VARCHAR(36))")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &UrlRepository{db: db}, nil
 }
 
 func (repository *UrlRepository) Add(url domain.Url) error {
-	return errors.New("not implemented yet")
+	query, err := repository.db.Prepare("INSERT INTO url (url, code) VALUES (?, ?)")
+	if err != nil {
+		return err
+	}
+
+	defer query.Close()
+
+	_, err = query.Exec(url.Url, url.Code)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (repository *UrlRepository) FindById(id int) (*domain.Url, error) {
 	return nil, errors.New("not implemented yet")
 }
+
 func (repository *UrlRepository) FindByCode(code string) (*domain.Url, error) {
-	return nil, errors.New("not implemented yet")
+	url := domain.Url{}
+	err := repository.db.QueryRow("SELECT url FROM url WHERE code = ?", code).Scan(&url.Url)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			fmt.Println("No rows found.")
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	return &url, nil
 }
